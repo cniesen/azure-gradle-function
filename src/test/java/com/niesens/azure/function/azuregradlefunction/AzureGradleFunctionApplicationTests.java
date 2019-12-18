600f0890742ffae2aea83aa11d4e8f47d59169a2
Claus Niesen <cniesen@users.noreply.github.com>
2019-12-18 09:00:03 -0600

    Initial commit

---
+++ src/test/java/com/niesens/azure/function/azuregradlefunction/AzureGradleFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niesens.azure.function.azuregradlefunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureGradleFunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
